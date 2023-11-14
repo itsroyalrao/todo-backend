1e3c2f4632a1c6ab910a9c46901872f0f1cf5685
Mohit Yadav <my701319@gmail.com>
2023-11-14 16:23:32 +0530

    second commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,15 +7,40 @@ const signup = async (req, res) => {
 
     const user = await Auth.findOne({ email: email });
     if (user) {
-      res.json({ success: false, message: "User already exists" });
+      return res.json({ success: false, message: "User already exists" });
     } else {
       bcrypt.hash(password, 10, async (err, encrypted) => {
         if (err) console.log(err);
 
         await Auth.create({ name, email, password: encrypted });
       });
-      res.json({ success: true });
+      return res.json({ success: true });
     }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Auth.findOne({ email: email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (err) console.log(err);
+        else if (same) return res.json({ success: true });
+        else
+          return res.json({
+            success: false,
+            message: "Password is incorrect!",
+          });
+      });
+    } else
+      return res.json({
+        success: false,
+        message: "User doesn't exist!",
+      });
   } catch (e) {
     console.log(e);
   }
