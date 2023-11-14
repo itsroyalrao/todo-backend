5782a450331aa210890850d7a70fdd24ee004648
Mohit Yadav <my701319@gmail.com>
2023-11-14 11:56:57 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 console.clear();
 
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.json({ msg: "Hello World!" }));
+app.get("/", (req, res) => res.json({ msg: "Mahadev" }));
+
+(async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://mohit:hPjYrJmEmFQ2HN9@nodeexpressprojects.t2ll3pf.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("Connected to database..."))
+    .catch((err) => console.log(err));
+})();
 app.listen(port, () => console.log(`Server listening on port ${port}`));
