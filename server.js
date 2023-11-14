0e69489962c5339ec5ce6a33239c7bbcbadc9613
Mohit Yadav <my701319@gmail.com>
2023-11-14 12:25:06 +0530

    seventh commit

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ console.clear();
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.json({ msg: "Mahadev" }));
+app.use("/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI).catch((err) => console.log(err));
 app.listen(port, () => console.log(`Server listening on port ${port}`));
