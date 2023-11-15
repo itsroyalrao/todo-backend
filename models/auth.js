313169bc11b4757c2ba848042497815ed8e7249a
Mohit Yadav <my701319@gmail.com>
2023-11-15 09:22:27 +0530

    fifth commit

--- models/auth.js
+++ models/auth.js
@@ -14,6 +14,10 @@ const authSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  loggedIn: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 export default mongoose.model("auth", authSchema);
