b83e210c80a0bf5d18199822a2c753e6fa366564
Mohit Yadav <my701319@gmail.com>
2023-11-16 21:30:17 +0530

    twenty-third commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+import nodemailer from "nodemailer";
 import bcrypt from "bcrypt";
 import Auth from "../models/auth.js";
 
@@ -70,4 +71,47 @@ const status = async (req, res) => {
   }
 };
 
-export { signup, login, logout, status };
+const resetPassword = async (req, res) => {
+  const { email } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "my701319@gmail.com",
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  let message = {
+    from: "my701319@gmail.com",
+    to: email,
+    subject: "Reset Password Link",
+    html: `<h4>Click <a href="https://todo-v0.netlify.app/changePassword?email=${email}">Here</a> to change your password.</h4>`,
+  };
+
+  try {
+    const info = await transporter.sendMail(message);
+    if (info.accepted.length)
+      return res.json({ success: true, msg: "Email is sent!" });
+  } catch (e) {
+    console.log(e);
+    return res.json({ success: false, msg: "Error occured!" });
+  }
+};
+
+const changePassword = async (req, res) => {
+  try {
+    const { email, newPassword } = req.body;
+
+    bcrypt.hash(newPassword, 10, async (err, encrypted) => {
+      if (err) console.log(err);
+
+      await Auth.findOneAndUpdate({ email: email }, { password: encrypted });
+      return res.json({ success: true, msg: "Password changed successfully!" });
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export { signup, login, logout, status, resetPassword, changePassword };
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { signup, login, logout, status } from "../controllers/auth.js";
+import {
+  signup,
+  login,
+  logout,
+  status,
+  resetPassword,
+  changePassword,
+} from "../controllers/auth.js";
 
 const router = express.Router();
 
@@ -7,5 +14,7 @@ router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
 router.route("/status").post(status);
+router.route("/resetPassword").post(resetPassword);
+router.route("/changePassword").post(changePassword);
 
 export default router;
